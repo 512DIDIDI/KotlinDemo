103c932c61e426666cfc603c9bce2a426664e584
dididi(yechao) <yc512yc@163.com>
2020-07-21 23:36:07 +0800

    feat self type

--- app/src/main/java/com/dididi/kotlindemo/class/class.md
+++ app/src/main/java/com/dididi/kotlindemo/class/class.md
@@ -1,30 +1,21 @@
 ### 类与对象
 
 * [类与接口](Class.kt)
-
   1. 抽象类
   2. 接口
   3. `open`关键字
   4. 构造器
   5. 属性`property`
 * [拓展方法](ExtFunction.kt)
-
   1. 实现方式
   2. 调用
 * [空类型安全](NullSafe.kt)
-
   1. `?`空类型判断
-
   2. `!!`不为空类型
-
   3. `?:` elvis语法
-
   4. 平台类型
-
   5. `is` 类型判断
-
   6. `as`/`as?` 强制类型转换
-
 * [构造方法](Constructor.kt)
   1. 主构造器/副构造器
   2. 成员变量
@@ -35,7 +26,6 @@
   2. `lateinit var`
   3. `by lazy`
 * [权限控制](Visibility.kt)
-
   1. `public` / `internal` / `protected` / `private`
 * [接口代理与属性代理](Delegate.kt)
   1. 接口代理
---
+++ app/src/main/java/com/dididi/kotlindemo/generic/SelfType.kt
@@ -0,0 +1,77 @@
+package com.dididi.kotlindemo.generic
+
+
+/**
+ * @author dididi(叶超)
+ * @email 512dididi@gmail.com
+ * @since 21/07/2020
+ * @describe 模拟Scala self-type
+ */
+
+typealias OnConfirm = () -> Unit
+typealias OnCancel = () -> Unit
+
+val EmptyFunction = fun() {}
+
+//模拟Self type，实现返回this
+interface SelfType<Self> {
+    val self: Self
+        get() = this as Self
+}
+
+open class Notification(
+    val title: String,
+    val content: String
+)
+
+class ConfirmNotification(
+    title: String,
+    content: String,
+    val onConfirm: OnConfirm,
+    val onCancel: OnCancel
+) : Notification(title, content)
+
+open class NotificationBuilder<Self:NotificationBuilder<Self>>:SelfType<Self> {
+    protected var title: String = ""
+    protected var content: String = ""
+    fun title(title: String): Self {
+        this.title = title
+        return self
+    }
+
+    fun content(content: String): Self {
+        this.content = content
+        return self
+    }
+
+    open fun build() = Notification(title, content)
+}
+
+class ConfirmNotificationBuilder : NotificationBuilder<ConfirmNotificationBuilder>() {
+    private var onConfirm: OnConfirm = EmptyFunction
+    private var onCancel: OnCancel = EmptyFunction
+
+    fun onConfirm(onConfirm: OnConfirm): ConfirmNotificationBuilder {
+        this.onConfirm = onConfirm
+        return this
+    }
+
+    fun onCancel(onCancel: OnCancel): ConfirmNotificationBuilder {
+        this.onCancel = onCancel
+        return this
+    }
+
+    override fun build() = ConfirmNotification(title, content, onConfirm, onCancel)
+}
+
+fun main() {
+    ConfirmNotificationBuilder()
+        .title("confirm message")
+        .content("are you confirm?")
+        .onConfirm {
+            println("onConfirm")
+        }
+        .build()
+        .onConfirm()
+}
+
